ee242db0cf92d1e6de8ae6b4dc96e25af8bb5e16
Ben Gruey <91962409+bgruey@users.noreply.github.com>
2024-05-14 09:54:25 -0500

    pkg updates

--- pkg/sqs/receive_message.go
+++ pkg/sqs/receive_message.go
@@ -8,27 +8,35 @@ import (
 )
 
 func (s *sqsClient) ReceiveMessages(timeout int32, max_messages int32) (messages []types.Message, err error) {
-	msgResult, err := s.svc.ReceiveMessage(
-		context.TODO(),
-		&sqs.ReceiveMessageInput{
-			MessageSystemAttributeNames: []types.MessageSystemAttributeName{
-				types.MessageSystemAttributeNameAll,
-			},
-			MessageAttributeNames: []string{
-				string(types.MessageSystemAttributeNameAll),
-			},
-			QueueUrl:            s.QueueURL,
-			MaxNumberOfMessages: *aws.Int32(max_messages),
-			VisibilityTimeout:   timeout,
-			WaitTimeSeconds:     *aws.Int32(20),
-		},
-	)
-
-	if err != nil {
-		return
+	iterations := max_messages / 10
+	if max_messages%10 != 0 {
+		iterations += 1
 	}
+	var msgResult *sqs.ReceiveMessageOutput
 
-	messages = msgResult.Messages
+	for i := 0; i < int(iterations); i++ {
+		msgResult, err = s.svc.ReceiveMessage(
+			context.TODO(),
+			&sqs.ReceiveMessageInput{
+				MessageSystemAttributeNames: []types.MessageSystemAttributeName{
+					types.MessageSystemAttributeNameAll,
+				},
+				MessageAttributeNames: []string{
+					string(types.MessageSystemAttributeNameAll),
+				},
+				QueueUrl:            s.QueueURL,
+				MaxNumberOfMessages: *aws.Int32(10),
+				VisibilityTimeout:   timeout,
+				WaitTimeSeconds:     *aws.Int32(20),
+			},
+		)
+
+		if err != nil {
+			return
+		}
+
+		messages = append(messages, msgResult.Messages...)
+	}
 
 	//for _, msg := range msgResult.Messages {
 	//	fmt.Printf("Message ID: %s\n", *msg.MessageId)
